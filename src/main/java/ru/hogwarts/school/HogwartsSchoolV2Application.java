b0dcfb025b9b89218d0d7719cd98d1143cd7287f
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-07-15 23:25:11 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/HogwartsSchoolV2Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsSchoolV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsSchoolV2Application.class, args);
+    }
+
+}
