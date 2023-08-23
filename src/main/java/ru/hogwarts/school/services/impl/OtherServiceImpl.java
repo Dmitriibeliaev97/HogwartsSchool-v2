14b66eac232fcb4efa95291b0155c1893ae1be7f
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-08-23 22:56:45 +0300

    HW 4.5 обновил метод getInteger. Измерение скорости

--- src/main/java/ru/hogwarts/school/services/impl/OtherServiceImpl.java
+++ src/main/java/ru/hogwarts/school/services/impl/OtherServiceImpl.java
@@ -9,12 +9,13 @@ import java.util.stream.Stream;
 public class OtherServiceImpl implements OtherService {
     @Override
     public Integer getInteger () {
+        long startTime = System.currentTimeMillis();
         int sum = Stream.iterate(1, a -> a + 1)
 //                .parallel()
-                .peek(it -> System.out.printf("Thread [%s] peek: %d\n",
-                        Thread.currentThread().getName(), it))
                 .limit(1_000_000)
                 .reduce(0, (a, b) -> a + b);
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println (estimatedTime);
         return sum;
     }
 }
